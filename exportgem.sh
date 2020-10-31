43baccde0b95cd21d9af8ddeb0c3de3064e08679
robertburrowes <r.burrowes@auckland.ac.nz>
2020-10-31 15:27:03 +1300

    Move dev scripts to sbin

--- exportgem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#Copy up to rubygem.org
-/usr/local/bin/rake release VERSION=1.1.0 #--trace
-
--- genpkg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#Local checking. Creates pkg/
-./gendoc.sh #Ensure History file is uptodate
-/usr/local/bin/rake --trace gem
-
